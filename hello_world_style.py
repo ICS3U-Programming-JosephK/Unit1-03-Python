9520293bc04213700615d4462f8c2b363397fe89
JosephK2006 <jiyong.kwon@stu.ocsb.ca>
2022-09-16 01:24:06 +0000

    added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Ms Raffin borrowed by Joseph 
+# Date: Sep. 15, 2022
+# This program displays Hello, World! with proper style.
+
+
+def main():
+    print("Hello, World! with proper style!")
+    print("Booga Booga")
+
+
+if __name__ == "__main__":
+    main()
